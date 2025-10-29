cd30db905e906c65073cb8f4372793deed95c1c7
Eric Albert Sabino Pinho <eric.uellendahl@gmail.com>
2025-10-29 16:01:46 -0300

    fix: update in currency

--- src/app/pages/check-out/check-out.ts
+++ src/app/pages/check-out/check-out.ts
@@ -1,9 +1,10 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './check-out.html',
   styleUrl: './check-out.css'
 })
